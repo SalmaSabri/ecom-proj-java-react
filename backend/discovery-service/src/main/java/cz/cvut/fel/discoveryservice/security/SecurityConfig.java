caed0bed3d370fedef0ca1461cd9cb6c35a7bb19
Aivazian, Artem <aivazart@fel.cvut.cz>
2024-09-15 16:20:53 +0200

    feat: Created, configured and secured discovery server
    FEA-11 Completed

---
+++ backend/discovery-service/src/main/java/cz/cvut/fel/discoveryservice/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package cz.cvut.fel.discoveryservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic(httpBasic -> httpBasic
+                        .realmName("DiscoveryRealm")
+                )
+                .build();
+    }
+}
