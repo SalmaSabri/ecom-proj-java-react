ba0e628ef9997d97c26e89f203896a0956ae9a89
Aivazian, Artem <aivazart@fel.cvut.cz>
2024-09-23 13:17:27 +0200

    feat: Add swagger for each module
    FEA-18 Completed

---
+++ backend/inventory-service/src/main/java/cz/cvut/fel/openapi/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package cz.cvut.fel.openapi;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI inventoryServiceAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Inventory Service API")
+                        .description("This is the REST API for managing products")
+                        .version("v1.0.0")
+                        .termsOfService("https://example.com/terms")
+                        .contact(new Contact()
+                                .name("Support Team")
+                                .email("aivazart@fel.cvut.cz"))
+                        .license(new License().name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .servers(Arrays.asList(
+                new Server().url("http://localhost:8010/inventory-service").description("Local Development Server"),
+                new Server().url("https://aivazart.com/inventory-service").description("Production Server")
+        ));
+    }
+}
